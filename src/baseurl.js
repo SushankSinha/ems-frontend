4c3e54cd18968ce96555a43fbdbfd384e239505c
sushanksinha <sushanksinha.5252@gmail.com>
2025-01-06 01:08:21 +0530

    update baseUrl

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  // baseURL: "http://localhost:5000/api"
+  baseURL: "https://ems-backend-s080.onrender.com/api"
 });
 
 instance.interceptors.request.use(
